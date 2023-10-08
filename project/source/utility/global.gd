16ccbd988682bcdffd4a5293e75d9bef1ad7d24e
WilsonVinson <cprewilsonvinson@gmail.com>
2023-10-08 11:51:44 +0800

    main

--- project/source/utility/global.gd
+++ project/source/utility/global.gd
@@ -1,4 +1,25 @@
 
 extends Node
 
-var night_node = false
+var fullscreen = true
+
+var night_node = true
+
+func _ready():
+	DisplayServer.window_set_title("花生迷途 Peanut's Journey")
+	
+	# print(DisplayServer.window_get_vsync_mode(0))
+	# DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	# print(DisplayServer.window_get_vsync_mode(0))
+	
+	DisplayServer.window_set_size(Vector2(1280,720))
+	DisplayServer.window_set_position(Vector2(100,100))
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	# DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN
+	
+	pass
+
+func _physics_process(delta):
+	# print(Engine.get_frames_per_second())
+	pass
+	
