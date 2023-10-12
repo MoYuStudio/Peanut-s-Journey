2d757d4f474c00c0689000bc26cffb7919bac23b
WilsonVinson <cprewilsonvinson@gmail.com>
2023-10-12 15:43:36 +0800

    main

--- project/source/player/car.gd
+++ project/source/player/car.gd
@@ -57,6 +57,7 @@ func collide(delta):
 	var collision = move_and_collide(vel * delta)
 	if collision:
 		vel = vel.slide(collision.get_normal()) # 使用super.lerp(vel.bounce(collision.normal), 0.1)来滑行
+		
 		rotation = lerp_angle(rotation, vel.angle(), 0.1)
 	
 func apply_friction():
